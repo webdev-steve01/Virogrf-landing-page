b58dc0d76e8dae18561b0406a40e63bbf4e85701
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2025-11-15 12:39:46 +0100

    update spot number

--- src/components/progress-bar/ProgressBar.tsx
+++ src/components/progress-bar/ProgressBar.tsx
@@ -6,7 +6,7 @@ import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 const TOTAL_SPOTS = 500;
-const BASE_COUNT = 325; // starting point
+const BASE_COUNT = 300; // starting point
 
 function ProgressBar() {
   const [count, setCount] = useState<number | null>(null);
